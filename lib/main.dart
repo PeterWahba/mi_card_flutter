997fc700f55e43e33caaafba2c123d9100c16189
Peter <peter.mounir4@gmail.com>
2020-12-20 16:56:42 +0200

    add stateless

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,47 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              width: 100.0,
+              color: Colors.red,
+            ),
+            Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 100.0,
+                    height: 100.0,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    width: 100.0,
+                    height: 100.0,
+                    color: Colors.green,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: 100.0,
+              color: Colors.blue,
+            ),
+          ],
+        )),
       ),
-    ),
-  );
+    );
+  }
 }
